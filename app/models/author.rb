560f23833a564fe0cc1e5843e37f97a01ed52a9c
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-03-21 15:44:12 -0400

    commit tree bf1c30033092887c296dec9f6da4dfa4f4eabe9b

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,13 @@
 class Author < ApplicationRecord
+    #author has a name
+    validates :name, presence: true
+
+    #no two authors have same name
+    validates :name, uniqueness: true
+
+    #author phone numbers are excalty  10 digits and is a #
+    validates :phone_number, length: { is: 10 }, numericality: true
+    
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+
+    validates :title, :content, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    # validates :category, inclusion: { in: %w(Fiction Non-Fiction) } is same below
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] } 
+    validates :title, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", 
+        "Guess"] }
+
+    validate :click_baity_title
+    # validate :check_category,
+    
+    def click_baity_title
+        click_baity_titles = ["Won't Believe", "Secret", "Top [number]", 
+        "Guess"]
+        # byebug
+        # click_baity_titles.each { |cb_title| title.downcase.include?( cb_title.downcase ) ? true :
+        #     self.errors.add("Title not click baity enough!") }
+    end
+
+    def check_category
+        unless self.category == "Fiction" or self.category == "Non-Fiction" 
+            self.errors.add("Category must be Fiction or Non-Fiction.")
+        end
+        # byebug
+    end
+
 end
